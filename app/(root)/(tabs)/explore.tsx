93b2507e14296fe602185b0a701c00f29d3f4b5a
Isola Pelumi <pelumiisola87@gmail.com>
2025-01-12 23:23:53 +0100

    feat: project setup

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Image, Platform, SafeAreaView, Text } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
